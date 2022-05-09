1e08617c812339ed0b9ec90b2b030cba3b2d4d29
fusbudget <andrewsanser@gmail.com>
2022-05-09 20:28:14 +0200

    add page package for routing;add Home, Contact, About, Projects routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
+  import router from 'page';
+  // components 
   import Navbar from './components/Navbar.svelte';
+  import Home from './components/Home.svelte';
+  import About from './components/About.svelte';
+  import Contact from './components/Contact.svelte';
+  import Projects from './components/Projects.svelte';
+
+  let page;
+  //routes 
+  router('/', () => page = Home);
+  router('/about', () => page = About);
+  router('/contact', () => page = Contact);
+  router('/projects', () => page = Projects);
+
+  router.start();
 </script>
 
+<Navbar />
 <main class="text-center p-4 mx-0">
-  <Navbar />
-  <h1 class="text-red-200">My portfolio</h1>  
+  <svelte:component this={page}  />
 </main>
+
 
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -10,31 +10,26 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Home</a></li>
-        <li><a>About</a></li>
-        <li><a>Contact</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </div>
 
   </div>
-  <!-----------      LOGO      ---------->
-  <!-- <div class=" lg:flex border border-solid border-blue">
-    <div class="hidden md:block md:w-16">
-      <a href="/" >
-        <img {src} alt="logo" />
-      </a>
-    </div>
-  </div> -->
+  
+  
   <div class="navbar-center hidden lg:flex">
 
       <ul class="menu menu-horizontal p-0">
         <li><a href="/">Home</a></li>
-        <li><a>About</a></li>
-        <li><a>Projects</a></li>
-        <li><a>Contact</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/projects">Projects</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
   </div>
   <div class="navbar-end">
+    <!-----------      LOGO      ---------->
     <div class="hidden md:block md:w-14 mr-8">
       <a href="/" >
         <img {src} alt="logo"/>
