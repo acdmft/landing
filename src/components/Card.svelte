7ae72a600d161126319b02d7d77025273d0b90ec
fusbudget <andrewsanser@gmail.com>
2022-05-10 20:03:41 +0200

    add Projects page and Card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  // image source and margins of the card
+  export let src;
+  export let margins;
+  // title of the card
+  export let title;
+  //card's text
+  export let text;
+  // card's badges
+  export let badges;
+  // project's links
+  export let gitLink;
+  export let deplLink;
+</script>
+
+<div class="card w-70 md:w-1/4 bg-slate-500 shadow-xl {margins}">
+  <figure class="h-30"><img {src} alt="Weather app" /></figure>
+  <div class="card-body px-2 py-4 md:px-4 md:py-4">
+    <h2 class="card-title text-base md:text-lg">{title}</h2>
+    <p class="text-left text-sm md:text-base">{text}</p>
+    <div class="card-actions justify-end">
+      <a href={gitLink} target="_blank" class="card-link leading-tight" 
+        >View source</a
+      >
+      |
+      <a href={deplLink} target="_blank" class="card-link leading-tight">Live demo</a>
+    </div>
+    <div class="card-actions justify-end">
+      {#if badges.length > 0}
+        {#each badges as badge}
+        <div class="badge badge-outline">{badge}</div>
+        {/each}
+      {/if}
+    </div>
+  </div>
+</div>
+
+<style>
+  .card-link {
+    padding: 0.2rem 0;
+    font-size: 0.9rem;
+    letter-spacing: 0;
+    cursor: pointer;
+    color: white;
+    text-decoration: none;
+    background-image: linear-gradient(120deg, #ff7ac6 100%, #ff7ac6 100%);
+    background-repeat: no-repeat;
+    background-size: 100% 0.2em;
+    background-position: 0 88%;
+    transition: background-size 0.25s ease-in;
+  }
+
+  .card-link:hover {
+    background-size: 100% 88%;
+  }
+</style>
--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,1 +1,27 @@
-<h1>Projects</h1>
+<script lang="ts">
+  import Card from './Card.svelte';
+  // Weather App 
+  let weatherAppText = "SPA built with React and OpenWeather API. Shows the current weather of the selected city. Has favorites section where you can add the cities that are most interesting to you."; 
+  let weatherAppBadges = ["React", "Tailwindcss"];
+  // Memory Game
+  let memoryText = "Classic card game where you must find all paired cards. Made with React and Bootstrap. Components are written with JS classes, use lifecycle functions onMount, onDidUpdate and setState function.";
+  let memoryBadges = ["React", "Bootstrap"];
+  // Green meet 
+  let greenText = "Functional React application. Where user can create events and join events. Uses Express and Postgresql in the backand. Work in team of developers inspired by Agile method"
+  let greenBadges = ["React", "Express", "Postgresql", "Tailwindcss"]
+  // Apm radio 
+  let radioText = "Online radio site with live chat. After connecting to the site (login or signup) user can participate in discussions in the chat. Built with Ruby on Rails. Uses websockets with the help of Action Cable and Redis."
+  let radioBadges = ["Ruby on Rails", "Node.js", "webpack", "redis"]
+</script>
+
+<h1 class="md:text-5xl text-xl font-semibold text-primary">Projects I've built</h1>
+<div class="w-5/6 mt-6 mx-auto flex flex-wrap jusify-around">
+  <div class="flex flex-wrap">
+    <Card src={"/weather-app.png"} margins={'md:ml-4 mx-auto mt-4 flex-none shrink-0'} title={"Weather App"} text={weatherAppText} badges={weatherAppBadges} gitLink="https://github.com/acdmft/weather-app" deplLink="https://acdmft-weather-app.netlify.app/" />
+    <Card src={"/memory.png"} margins={'md:ml-auto mx-auto mt-4 flex-none srink-0'} title={"Memory Game"} text={memoryText}  badges={memoryBadges} gitLink="https://github.com/acdmft/memory-game" deplLink="https://acdmf-memo-game.netlify.app/" />
+    <Card src={"/meet.png"} margins={'md:ml-auto mx-auto mt-4 md:mr-4 flex-none srink-0'} title={"Green Meet"} text={greenText} badges={greenBadges} gitLink="https://github.com/Green-Meet" deplLink="#" />
+    <Card src={"/radio.png"} margins={'md:ml-4 mx-auto mt-4 flex-none'} title={"Apm Radio"} text={radioText} badges={radioBadges} gitLink="https://bitbucket.org/stgmft/apm_radio/src/master/" deplLink="https://apm-radio.herokuapp.com/" />
+  </div>
+
+</div>
+<div class="h-10"></div>
