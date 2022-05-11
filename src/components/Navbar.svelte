e12a30e8c1d21c9a87b551e4f90c579eb9030600
fusbudget <andrewsanser@gmail.com>
2022-05-11 15:46:35 +0200

    add projects point in dropdown menu

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -12,6 +12,7 @@
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
+        <li><a href="/projects">Projects</a></li>
         <li><a href="/contact">Contact</a></li>
       </ul>
     </div>
